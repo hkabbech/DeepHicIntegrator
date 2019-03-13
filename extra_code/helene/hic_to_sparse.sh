5a191fdd65eff0f0d787ce1705c2c9d0a0400fc1
kabhel <helene.kabbech@gmail.com>
2019-03-13 16:04:28 +0100

    Add sed command line

--- extra_code/helene/hic_to_sparse.sh
+++ extra_code/helene/hic_to_sparse.sh
@@ -1,4 +1,4 @@
 echo "chromosome: $1"
 echo "file: $2"
-juicetools dump observed KR $2 $1 $1 BP 25000 $1_ihic.txt
-	
+juicetools dump observed KR $2 $1 $1 BP 25000 $1.txt
+sed -i 's/^.*NaN.*\n//g;s/^.*NaN\n//g;s/^/chr20\t/g' $1.txt
