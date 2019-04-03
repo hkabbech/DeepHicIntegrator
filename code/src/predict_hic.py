aef5f35b29ac90ac31d91e3c9494714428444b1f
kabhel <helene.kabbech@gmail.com>
2019-04-03 19:08:18 +0200

    POO, triangular matrix, Doc, cooler, etc. Done.

---
+++ code/src/predict_hic.py
@@ -0,0 +1,109 @@
+"""
+.. module:: PredictHic
+   :synopsis: This module implements the PredictHic class.
+"""
+
+# Third-party modules
+import csv
+import numpy as np
+from scipy.sparse import coo_matrix
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+# Parent Hic class
+from src.hic import Hic
+
+
+class PredictHic(Hic):
+    """
+    .. class:: PredictHic
+        This class herits attributes and methods from Hic class and
+        groups informations about a Hi-C matrix.
+
+    Attributes:
+        predicted_sub_matrices (numpy array): Sub-matrices predicted by a deep-learning model
+        predicted_matrix (numpy array): The reconstructed matrix from the predicted sub-matrices
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.predicted_sub_matrices = None
+        self.predicted_matrix = None
+
+    def set_predicted_sub_matrices(self, predicted_sub_matrices):
+        """
+            Set the sub-matrices predicted by a deep-learning model.
+        """
+        self.predicted_sub_matrices = predicted_sub_matrices
+
+    def construct_predicted_matrix(self):
+        """
+            Construction and set of the predicted Hi-C matrix from the predicted sub-matrices.
+        """
+        white_sub_matrix = np.zeros(shape=(self.side, self.side, 1))
+        nb_sub_matrices = int(self.matrix.shape[0] / self.side)
+        nb_white = 0
+        j = 1
+        for _, sub_matrix in enumerate(self.predicted_sub_matrices):
+            if j == nb_sub_matrices - 1:
+                try:
+                    line = np.concatenate((line, sub_matrix), axis=1)
+                    predicted_matrix = np.concatenate((predicted_matrix, line), axis=0)
+                except NameError:
+                    predicted_matrix = line
+                nb_white += 1
+                j = 1
+                del line
+            else:
+                try:
+                    line = np.concatenate((line, sub_matrix), axis=1)
+                except NameError:
+                    if nb_white == 0:
+                        line = sub_matrix
+                    else:
+                        line = white_sub_matrix
+                        for i in range(1, nb_white):
+                            line = np.concatenate((line, white_sub_matrix), axis=1)
+                            j += 1
+                        line = np.concatenate((line, sub_matrix), axis=1)
+                        j += 1
+                j += 1
+        self.predicted_matrix = predicted_matrix.reshape(predicted_matrix.shape[0],
+                                                         predicted_matrix.shape[1])
+
+    def plot_ten_predicted_submatrices(self, color_map, output_path, index_list):
+        """
+            10 predicted sub-matrices are plotted in a file.
+        """
+        plt.figure(figsize=(18, 2))
+        for i, index in enumerate(index_list):
+            plt.subplot(1, 10, i+1)
+            plt.imshow(self.predicted_sub_matrices[index, ..., 0], cmap=color_map)
+            plt.title("submatrix n°{}".format(index))
+        plt.subplots_adjust(left=0.03, right=0.98, wspace=0.3)
+        plt.savefig('{}/10submatrices_chr{}_predicted.png'.format(output_path, self.chrom))
+
+    def plot_predicted_matrix(self, color_map, output_path):
+        """
+            The reconstructed and predicted Hi-C matrix is plotted in a file. 
+        """
+        fig = plt.figure(figsize=(12, 12))
+        axes = plt.subplot(111, aspect='equal')
+        img = axes.matshow(self.predicted_matrix, cmap=color_map)
+        divider = make_axes_locatable(axes)
+        cax = divider.append_axes("right", size="2%", pad=0.15)
+        plt.colorbar(img, cax=cax)
+        plt.subplots_adjust(left=0.07, bottom=0, right=0.95, top=0.91, wspace=0, hspace=0)
+        axes.set_title('Predicted chr{} Hi-C matrix'.format(self.chrom), fontsize=25)
+        fig.savefig('{}/chr{}_predicted.png'.format(output_path, self.chrom))
+
+    def write_predicted_matrix(self, threshold, output_path):
+        """
+            The reconstructed and predicted Hi-C matrix is saved in a sparse matrix file.
+        """
+        self.predicted_matrix[self.predicted_matrix < threshold] = 0
+        sparse = coo_matrix(self.predicted_matrix)
+        with open(output_path+'/predicted_chr'+str(self.chrom)+'.txt', 'w') as file:
+            writer = csv.writer(file, delimiter='\t')
+            writer.writerows(zip(['chr'+str(self.chrom)]*len(sparse.row), sparse.row*self.resolution,
+                                 sparse.col*self.resolution, sparse.data))
