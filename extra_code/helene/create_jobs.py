179f06b63b8dd37d5c4d989c9381d242c5e94844
kabhel <helene.kabbech@gmail.com>
2019-04-10 18:27:44 +0200

    Cluster : Try diff resolution

--- extra_code/helene/create_jobs.py
+++ extra_code/helene/create_jobs.py
@@ -5,18 +5,18 @@ import os
 
 os.makedirs('jobs/', exist_ok=True)
 
-for i in range(1, 11):
-	os.makedirs('results/train_'+str(i), exist_ok=True)
-	with open('jobs/job_train_'+str(i)+'.sh', 'w') as filout:
+for i in [2500000, 1000000, 500000, 250000, 100000, 50000, 25000, 10000, 5000]:
+	os.makedirs('results/resolution_'+str(i), exist_ok=True)
+	with open('jobs/job_resolution_'+str(i)+'.sh', 'w') as filout:
 		filout.write('#!/bin/bash\n')
 		filout.write('#BSUB -q gpu\n')
 		filout.write('#BSUB -W 10:00\n')
-		filout.write('#BSUB -R "ngpus=2"\n')
-		filout.write('#BSUB -o results/train_'+str(i)+'/out-%J.txt\n')
-		filout.write('#BSUB -e results/train_'+str(i)+'/err-%J.txt\n\n')
+		filout.write('#BSUB -R "ngpus=3"\n')
+		filout.write('#BSUB -o results/resolution_'+str(i)+'/out-%J.txt\n')
+		filout.write('#BSUB -e results/resolution_'+str(i)+'/err-%J.txt\n\n')
 
 		filout.write('module load cuda90/toolkit/9.0.176\n')
 		filout.write('module load cuda90/blas/9.0.176\n')
 		filout.write('module load cudnn/90v7.3.1\n\n')
 
-		filout.write('./deep_hic_integrator data/1_binaries/hic/GSE63525_HUVEC_combined_30.hic -o results/train_'+str(i)+'/ --test 20 --train '+str(i)+'\n')
+		filout.write('./deep_hic_integrator data/1_binaries/hic/GSE63525_HUVEC_combined_30.hic -o results/resolution_'+str(i)+'/ --test 20 --train 1 --resolution '+str(i)+'\n')
